1d07c0d1f8f33fa8b8639f6e7526aae2d411b0d0
RND-HW3 <RND-HW3@RND-HW3>
2025-05-02 15:36:58 +0900

    초기 커밋.

---
+++ UsingGenericQueue/MainApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UsingGenericQueue
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                queue.Enqueue(i);
+            }
+            foreach (int que in queue)
+            {
+                Console.Write($"{que} ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 3; i++)
+            {
+                queue.Dequeue();
+            }
+            foreach (int que in queue)
+            {
+                Console.Write($"{que} ");
+            }
+        }
+    }
+}
